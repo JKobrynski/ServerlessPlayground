3ad35c3cab177b3cd3558f500559817970b02d9e
Julian Kobrynski <julian.kobrynski@netguru.com>
2022-11-11 16:59:05 +0100

    graphql api added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "serverlessplayground": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
